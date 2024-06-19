fa8a9247355ef8785e8c808bc7cc0b82c385c231
Michail Fedyaev <drovosekov1910@mail.ru>
2024-06-19 12:34:50 +0500

    gitignore

--- src/masks.py
+++ src/masks.py
@@ -3,11 +3,8 @@ from typing import Any
 
 def mask_card_number(card_number: str) -> Any:
     """Функция, маскировки номера карты"""
-    masked_number = (
-        card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
-    )
+    masked_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
     return masked_number
-
 
 
 def mask_account(account_number: str) -> Any:
@@ -17,4 +14,3 @@ def mask_account(account_number: str) -> Any:
         return "Invalid account number"
     masked_number = "**" + account_number[-4:]
     return masked_number
-
--- src/widget.py
+++ src/widget.py
@@ -1,28 +1,24 @@
+import datetime
 from typing import Any
 
-import masks
-
-import datetime
-
+from src.masks import mask_account, mask_card_number
 
 
 def mask_account_card(string: str) -> Any:
-    if 'Счет' in string:
+    if "Счет" in string:
         account = string[5:]
-        return 'Счет' + ' ' + masks.mask_account(account)
+        return "Счет" + " " + mask_account(account)
     else:
-        card_number = ''.join(string[-16:].split())
-        return string[:-16] + masks.mask_card_number(card_number)
-
+        card_number = "".join(string[-16:].split())
+        return string[:-16] + mask_card_number(card_number)
 
 
 def get_data(string: str) -> Any:
     current_time = datetime.datetime.now()
-    get_current_time = f'{current_time.strftime('%d.%m.%Y')}'
+    get_current_time = f"{current_time.strftime('%d.%m.%Y')}"
     return get_current_time
 
 
-print(mask_account_card('Счет 73654108430135874305'))
-print(mask_account_card('MasterCard 7158300734726758'))
-print(get_data('2018-07-11T02:26:18.671407'))
-
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(get_data("2018-07-11T02:26:18.671407"))
