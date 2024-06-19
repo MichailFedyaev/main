f290720816b14b849589a365c8cb86451cd9bf13
Michail Fedyaev <drovosekov1910@mail.ru>
2024-06-19 13:04:58 +0500

    correct update widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from typing import Any
 
 from src.masks import mask_account, mask_card_number
@@ -13,10 +13,9 @@ def mask_account_card(string: str) -> Any:
         return string[:-16] + mask_card_number(card_number)
 
 
-def get_data(string: str) -> Any:
-    current_time = datetime.datetime.now()
-    get_current_time = f"{current_time.strftime('%d.%m.%Y')}"
-    return get_current_time
+def get_data(data: str) -> str:
+    time = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S.%f"))
+    return time.strftime("%d.%m.%Y")
 
 
 print(mask_account_card("Счет 73654108430135874305"))
