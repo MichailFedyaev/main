e6cd9981b2b2dceec17f6a46f8ce4415d0197c69
Michail Fedyaev <drovosekov1910@mail.ru>
2024-07-15 10:47:56 +0500

    update conftest.py and .coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ from src.processing import inform_state
 
 from src.generators import transactions
 
+from src.utils import get_transactions_dictionary
+
 
 @pytest.fixture()
 def test_inform_state():
@@ -23,3 +25,28 @@ def test_data():
 @pytest.fixture()
 def test_transactions():
     return transactions
+
+
+@pytest.fixture()
+def get_path():
+    return "D:/PycharmProject/main/data/operations.json"
+
+
+@pytest.fixture()
+def get_wrong_path():
+    return "Nothing"
+
+
+@pytest.fixture()
+def get_bad_file():
+    return '../data/wrong_operations.json'
+
+
+@pytest.fixture()
+def transaction():
+    return get_transactions_dictionary("D:/PycharmProject/main/data/operations.json")
+
+
+@pytest.fixture()
+def rub_transaction_number():
+    return 939719570
