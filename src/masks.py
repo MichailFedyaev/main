91d74b59bb1fc2e28b7e14ff349ec58989c110a3
Michail Fedyaev <drovosekov1910@mail.ru>
2024-07-17 14:58:46 +0500

    update masks and utils

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,14 @@
 from typing import Any
+from src.logger_setup import setup_logger
+
+# Создание и получение именованного логгера
+masks_logger = setup_logger(__name__)
 
 
 def mask_card_number(card_number: str) -> Any:
     """Функция, маскировки номера карты"""
     masked_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+    masks_logger.info(f"Masked card number: {masked_number}")
     return masked_number
 
 
@@ -13,4 +18,10 @@ def mask_account(account_number: str) -> Any:
     if not account_number.isdigit() or len(account_number) < 4:
         return "Invalid account number"
     masked_number = "**" + account_number[-4:]
+    masks_logger.info(f"Masked account number: {masked_number}")
     return masked_number
+
+
+
+#if __name__ == "__main__":
+    #print(mask_card_number('7000 7911 3321 6361'))
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,34 @@
 import json
 import os
 from typing import Any, List, Dict
+from src.logger_setup import setup_logger
+
+# Создание и получение именованного логгера
+utils_logger = setup_logger(__name__)
 
 
 def get_transactions_dictionary(file_path: str) -> List[Dict[str, Any]]:
     """Функция принимает путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
     if not os.path.exists(file_path):
+        utils_logger.warning(f"File {file_path} does not exist.")
         return []
 
     try:
         with open(file_path) as operations:
             transaction_data = json.load(operations)
             if isinstance(transaction_data, list):
+                utils_logger.info(f"Successfully read file: {file_path}")
                 return transaction_data
             else:
+                utils_logger.warning(f"Invalid data format in file: {file_path}")
                 return []
 
-    except (json.JSONDecodeError, IOError):
+    except (json.JSONDecodeError, IOError) as e:
+        utils_logger.error(f"Error reading file {file_path}: {e}")
         return []
+
+
+#if __name__ == "__main__":
+    #В лог записывается что такого файла не существует)
+    #transact = get_transactions_dictionary("D:/PycharmProject/non-existent/data/operations.json")
+    #print(transact)
