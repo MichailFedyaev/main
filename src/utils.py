6d80df38f325ebde9c91a6c770492f442c73f513
Michail Fedyaev <drovosekov1910@mail.ru>
2024-07-22 11:39:43 +0500

    update utils.py and test_utils.py (add get_transactions_csv and get_transactions_excel).

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 import json
 import os
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Hashable
 from src.logger_setup import setup_logger
+import pandas as pd
 
 # Создание и получение именованного логгера
 utils_logger = setup_logger(__name__)
@@ -25,4 +26,58 @@ def get_transactions_dictionary(file_path: str) -> List[Dict[str, Any]]:
 
     except (json.JSONDecodeError, IOError) as e:
         utils_logger.error(f"Error reading file {file_path}: {e}")
+        return []
+
+
+def get_transactions_csv(file_path: str) -> List[Dict[Hashable, Any]]:
+    """ Функция принимает путь до CSV-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    if not os.path.exists(file_path):
+        utils_logger.warning(f"File does not exist: {file_path}")
+        return []
+
+    try:
+        df = pd.read_csv(file_path)
+        if df.empty or not isinstance(df, pd.DataFrame):
+            utils_logger.warning(f"File is empty or not a DataFrame: {file_path}")
+            return []
+        utils_logger.info(f"Successfully read CSV file: {file_path}")
+        return df.to_dict(orient="records")
+
+    except pd.errors.EmptyDataError:
+        utils_logger.error(f"EmptyDataError: File is empty: {file_path}")
+        return []
+
+    except pd.errors.ParserError:
+        utils_logger.error(f"ParserError: Failed to parse file: {file_path}")
+        return []
+
+    except Exception as e:
+        utils_logger.error(f"Unexpected error: {e}")
+        return []
+
+
+def get_transactions_excel(file_path: str) -> List[Dict[Hashable, Any]]:
+    """ Функция принимает путь до EXEL-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    if not os.path.exists(file_path):
+        utils_logger.warning(f"File does not exist: {file_path}")
+        return []
+
+    try:
+        df = pd.read_excel(file_path)
+        if df.empty or not isinstance(df, pd.DataFrame):
+            utils_logger.warning(f"File is empty or not a DataFrame: {file_path}")
+            return []
+        utils_logger.info(f"Successfully read Excel file: {file_path}")
+        return df.to_dict(orient="records")
+
+    except pd.errors.EmptyDataError:
+        utils_logger.error(f"EmptyDataError: File is empty: {file_path}")
+        return []
+
+    except ValueError:
+        utils_logger.error(f"ValueError: Failed to parse file: {file_path}")
+        return []
+
+    except Exception as e:
+        utils_logger.error(f"Unexpected error: {e}")
         return []
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,11 @@
 from typing import List, Dict, Any
 import json
 from unittest.mock import mock_open, patch, Mock
-from src.utils import get_transactions_dictionary
+from src.utils import get_transactions_dictionary, get_transactions_csv, get_transactions_excel
+import pandas as pd
 
 
 def test_get_transactions_dictionary_valid_file(transaction1: List[Dict[str, Any]]) -> None:
-    """Тестирует функцию get_transactions_dictionary с существующим JSON-файлом, содержащим корректные данные."""
     # Преобразуем список транзакций в JSON-строку
     json_data = json.dumps(transaction1)
 
@@ -23,7 +23,6 @@ def test_get_transactions_dictionary_valid_file(transaction1: List[Dict[str, Any
 
 @patch("src.utils.utils_logger")
 def test_get_transactions_dictionary_logs_info(mock_logger: Mock, transaction1: List[Dict[str, Any]]) -> None:
-    """Тестирует, что функция get_transactions_dictionary логирует корректное сообщение при успешном чтении файла."""
     # Преобразуем список транзакций в JSON-строку
     json_data = json.dumps(transaction1)
 
@@ -40,8 +39,6 @@ def test_get_transactions_dictionary_logs_info(mock_logger: Mock, transaction1: 
 
 @patch("src.utils.utils_logger")
 def test_get_transactions_dictionary_invalid_format_logs_warning(mock_logger: Mock) -> None:
-    """Тестирует, что функция get_transactions_dictionary логирует сообщение предупреждение
-     при некорректном формате данных."""
     # Используем mock_open для имитации открытия файла и чтения некорректных данных
     mocked_open = mock_open(read_data='{"invalid": "data"}')
     with patch("builtins.open", mocked_open):
@@ -55,7 +52,6 @@ def test_get_transactions_dictionary_invalid_format_logs_warning(mock_logger: Mo
 
 
 def test_get_transactions_dictionary_invalid_file() -> None:
-    """Тестирует функцию get_transactions_dictionary с существующим JSON-файлом, содержащим некорректные данные."""
     # Используем mock_open для имитации открытия файла и чтения некорректных данных
     mocked_open = mock_open(read_data='{"invalid": "data"}')
     with patch("builtins.open", mocked_open):
@@ -69,7 +65,6 @@ def test_get_transactions_dictionary_invalid_file() -> None:
 
 
 def test_get_transactions_dictionary_empty_file() -> None:
-    """ Тестирует функцию read_transactions_json с существующим пустым JSON-файлом."""
     # Используем mock_open для имитации открытия пустого файла и чтения данных
     mocked_open = mock_open(read_data="")
     with patch("builtins.open", mocked_open):
@@ -83,7 +78,6 @@ def test_get_transactions_dictionary_empty_file() -> None:
 
 
 def test_get_transactions_dictionary_nonexistent_file() -> None:
-    """Тестирует функцию get_transactions_dictionary с несуществующим JSON-файлом."""
     # Используем patch для имитации os.path.exists и задаем, чтобы она возвращала False
     with patch("os.path.exists") as mock_exists:
         mock_exists.return_value = False
@@ -109,7 +103,6 @@ def test_get_transactions_dictionary_json_decode_error() -> None:
 
 
 def test_get_transactions_dictionary_json_io_error() -> None:
-    """Тестирует функцию read_transactions_json с существующим JSON-файлом при возникновении ошибки IOError."""
     # Используем контекстный менеджер path и аргумент side_effect для имитации открытия файла и генерации исключения
     with patch("builtins.open", side_effect=IOError):
         # Используем patch для имитации os.path.exists и задаем, чтобы она возвращала True
@@ -119,3 +112,120 @@ def test_get_transactions_dictionary_json_io_error() -> None:
             result = get_transactions_dictionary("dummy_path.json")
             assert result == []
             mock_exists.assert_called_once_with("dummy_path.json")
+
+
+@patch("src.utils.utils_logger")
+def test_get_transactions_csv_valid_file(mock_logger: Mock) -> None:
+    """ Тестирует функцию get_transactions_csv с существующим CSV-файлом, содержащим корректные данные."""
+    data = pd.DataFrame([{"id": 1, "amount": 100}, {"id": 2, "amount": 200}])
+
+    # Используем patch для имитации pandas.read_csv и os.path.exists
+    with patch("pandas.read_csv", return_value=data) as mock_read_csv:
+        with patch("os.path.exists", return_value=True):
+            # Вызываем тестируемую функцию и проверяем результат
+            result = get_transactions_csv("dummy_path.csv")
+            expected_result = data.to_dict(orient="records")
+            assert result == expected_result
+            mock_read_csv.assert_called_once_with("dummy_path.csv")
+            mock_logger.info.assert_called_once_with("Successfully read CSV file: dummy_path.csv")
+
+
+@patch("src.utils.utils_logger")
+def test_get_transactions_csv_empty_file(mock_logger: Mock) -> None:
+    """ Тестирует функцию get_transactions_csv с существующим пустым CSV-файлом."""
+    # Используем patch для имитации pandas.read_csv и возвращения пустого DataFrame
+    with patch("pandas.read_csv", return_value=pd.DataFrame()):
+        with patch("os.path.exists") as mock_exists:
+            mock_exists.return_value = True
+            # Вызываем тестируемую функцию и проверяем результат
+            result = get_transactions_csv("dummy_path.csv")
+            assert result == []
+            mock_logger.warning.assert_called_once_with("File is empty or not a DataFrame: dummy_path.csv")
+
+
+@patch("src.utils.utils_logger")
+def test_get_transactions_csv_parse_error(mock_logger: Mock) -> None:
+    """ Тестирует функцию get_transactions_csv с существующим CSV-файлом, содержащим некорректные данные."""
+    csv_data = "id,amount\n1,100\n2,"
+
+    # Используем mock_open для имитации открытия файла и чтения некорректных данных
+    mocked_open = mock_open(read_data=csv_data)
+    with patch("builtins.open", mocked_open):
+        with patch("os.path.exists") as mock_exists:
+            mock_exists.return_value = True
+            # Используем контекстный менеджер и аргумент side_effect для имитации чтения и генерации исключения
+            with patch("pandas.read_csv", side_effect=pd.errors.ParserError("Mock ParserError")):
+                # Вызываем тестируемую функцию и проверяем результат
+                result = get_transactions_csv("dummy_path.csv")
+                assert result == []
+                mock_logger.error.assert_called_once_with("ParserError: Failed to parse file: dummy_path.csv")
+
+
+@patch("src.utils.utils_logger")
+def test_get_transactions_csv_io_error(mock_logger: Mock) -> None:
+    """ Тестирует функцию get_transactions_csv с существующим CSV-файлом и имитацией IOError."""
+    # Используем контекстный менеджер pathc и аргумент side_effect для имитации открытия файла и генерации исключения
+    with patch("builtins.open", side_effect=IOError("Mock IOError")):
+        with patch("os.path.exists") as mock_exists:
+            mock_exists.return_value = True
+            # Вызываем тестируемую функцию и проверяем результат
+            result = get_transactions_csv("dummy_path.csv")
+            assert result == []
+            mock_logger.error.assert_called_once_with("Unexpected error: Mock IOError")
+
+
+@patch("src.utils.utils_logger")
+def test_get_transactions_excel_valid_file(mock_logger: Mock) -> None:
+    """ Тестирует функцию get_transactions_excel с существующим Excel-файлом, содержащим корректные данные."""
+    data = pd.DataFrame([{"id": 1, "amount": 100}, {"id": 2, "amount": 200}])
+
+    # Используем patch для имитации pandas.read_excel и os.path.exists
+    with patch("pandas.read_excel", return_value=data) as mock_read_excel:
+        with patch("os.path.exists", return_value=True):
+            # Вызываем тестируемую функцию и проверяем результат
+            result = get_transactions_excel("dummy_path.xlsx")
+            expected_result = data.to_dict(orient="records")
+            assert result == expected_result
+            mock_read_excel.assert_called_once_with("dummy_path.xlsx")
+            mock_logger.info.assert_called_once_with("Successfully read Excel file: dummy_path.xlsx")
+
+
+@patch("src.utils.utils_logger")
+def test_get_transactions_excel_empty_file(mock_logger: Mock) -> None:
+    """Тестирует функцию get_transactions_excel с существующим пустым Excel-файлом."""
+    empty_data = pd.DataFrame()
+
+    # Используем patch для имитации pandas.read_excel и os.path.exists
+    with patch("pandas.read_excel", return_value=empty_data) as mock_read_excel:
+        with patch("os.path.exists", return_value=True):
+            # Вызываем тестируемую функцию и проверяем результат
+            result = get_transactions_excel("dummy_empty.xlsx")
+            assert result == []
+            mock_read_excel.assert_called_once_with("dummy_empty.xlsx")
+            mock_logger.warning.assert_called_once_with("File is empty or not a DataFrame: dummy_empty.xlsx")
+
+
+@patch("src.utils.utils_logger")
+def test_get_transactions_excel_parse_error(mock_logger: Mock) -> None:
+    """Тестирует функцию get_transactions_excel с существующим Excel-файлом, содержащим некорректные данные."""
+    # Используем patch для имитации pandas.get_excel и генерации ValueError
+    with patch("pandas.read_excel", side_effect=ValueError("Mock ValueError")):
+        with patch("os.path.exists") as mock_exists:
+            mock_exists.return_value = True
+            # Вызываем тестируемую функцию и проверяем результат
+            result = get_transactions_excel("dummy_path.xlsx")
+            assert result == []
+            mock_logger.error.assert_called_once_with("ValueError: Failed to parse file: dummy_path.xlsx")
+
+
+@patch("src.utils.utils_logger")
+def test_get_transactions_excel_io_error(mock_logger: Mock) -> None:
+    """ Тестирует функцию get_transactions_excel с существующим Excel-файлом и имитацией IOError."""
+    # Используем контекстный менеджер pathc и аргумент side_effect для имитации чтения файла и генерации исключения
+    with patch("pandas.read_excel", side_effect=IOError("Mock IOError")):
+        with patch("os.path.exists") as mock_exists:
+            mock_exists.return_value = True
+            # Вызываем тестируемую функцию и проверяем результат
+            result = get_transactions_excel("dummy_path.xlsx")
+            assert result == []
+            mock_logger.error.assert_called_once_with("Unexpected error: Mock IOError")
